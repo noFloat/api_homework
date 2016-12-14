d4cd5ebde41e9a863ca5ca18d4719ab5131a2c9e
nofloat <304546210@qq.com>
2016-12-14 11:36:12 +0800

    bug

--- app/Http/Controllers/Content/BuildingController.php
+++ app/Http/Controllers/Content/BuildingController.php
@@ -34,6 +34,15 @@ class BuildingController extends Controller
 		$buildinginfo['e_value'] = $e_value;
 		$buildinginfo['d_value'] = $d_value;
 
+		$before_d_value = 0;
+		$before_e_value = 0;
+		foreach ($data_date as $key => $value) {
+			$before_d_value += $value['attributes']['before_dvalue'];
+			$before_e_value += $value['attributes']['before_evalue'];
+		}
+		$buildinginfo['before_e_value'] = $before_e_value;
+		$buildinginfo['before_d_value'] = $before_d_value;
+
 		$tody_data = Daydata::whereIn('eiid',$equipmentId)->orderBy('etime','DESC')
 		->skip(count($equipmentInfo))->take(count($equipmentInfo))->get();
 
@@ -43,8 +52,8 @@ class BuildingController extends Controller
 			$today_d_value += $value['attributes']['dvalue'];
 			$today_e_value += $value['attributes']['evalue'];
 		}
-		$buildinginfo['today_e_value'] = $e_value - $today_e_value;
-		$buildinginfo['today_d_value'] = $d_value - $today_d_value;
+		$buildinginfo['today_e_value'] = $today_e_value;
+		$buildinginfo['today_d_value'] = $today_d_value;
 
 		$info = [
 	            "status"  => 200,
@@ -57,11 +66,17 @@ class BuildingController extends Controller
 	public function energy_now(){
 		$energyData = new Energydata();
 		$data = $energyData->join('equipment', 'equipment.id', '=', 'energydatas.eiid')
-						   ->orderBy('etime','DESC')->take(15)->get();
-
+						   ->orderBy('energydatas.etime','DESC')->take(15)->get();
 		foreach ($data as $key => $value) {
 			$all_data[$key] = $value['attributes'];
 		}
+
+		$flag=array();
+		foreach($all_data as $key=>$value){
+		    	$id[$key] = $value['id'];
+    			$eiId[$key] = $value['eiId'];
+		    }
+		array_multisort($eiId,SORT_NUMERIC,SORT_ASC,$id,SORT_STRING,SORT_ASC,$all_data);
 		$info = [
 	            "status"  => 200,
 	            "info"    =>"success",
