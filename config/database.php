758f9219dd45bdd57c475dbaf9af05ba4af26546
nofloat <304546210@qq.com>
2017-01-15 22:39:19 +0800

    test

--- config/database.php
+++ config/database.php
@@ -77,6 +77,15 @@ return [
             'prefix' => '',
             'schema' => 'public',
         ],
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE_search', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+        ],
 
     ],
 
